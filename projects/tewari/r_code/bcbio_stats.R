7f7510448aa33866494fe7122b7b8f62d76c4401
Lorena Pantano <lorena.pantano@gmail.com>
2018-05-17 18:08:59 -0400

    update bcbio stats figures

--- projects/tewari/r_code/bcbio_stats.R
+++ projects/tewari/r_code/bcbio_stats.R
@@ -1,8 +1,10 @@
 setwd(here::here())
+library(tidyverse)
+theme_set(theme_bw())
 
 meta_pilot = read_csv("meta_pilot.csv")
 
-bcbio_stats = read_csv("tewari/bcbio/stats/mirtop_stats.txt", skip = 1) %>% 
+bcbio_stats = read_csv("tools/bcbio/stats/mirtop_stats.txt", skip = 1) %>% 
     mutate(sample=gsub("-mirbase-ready", "", sample)) %>% 
     inner_join(meta_pilot,
                by = c("sample" = "fixed_name"))
@@ -13,11 +15,28 @@ ggplot(bcbio_stats %>% filter(grepl("_count", category),
        aes(x = lab, y = counts, fill = as.factor(replicate))) +
     geom_bar(stat = "identity", position = "dodge") +
     facet_wrap(~category, scales = "free_y", nrow=4) +
-    ggsave("figures/stats/bcbio_count.pdf")
+    ggtitle("bcbio - TrueSeq") +
+    ggsave("figures/stats/bcbio_truseq_count.png")
+ggplot(bcbio_stats %>% filter(grepl("_count", category),
+                              lib_method_simple == "NEBNext"),
+       aes(x = lab, y = counts, fill = as.factor(replicate))) +
+    geom_bar(stat = "identity", position = "dodge") +
+    facet_wrap(~category, scales = "free_y", nrow=4) +
+    ggtitle("bcbio - NEBNext") +
+    ggsave("figures/stats/bcbio_nebnext_count.png")
+
 
 ggplot(bcbio_stats %>% filter(grepl("_sum", category),
                               lib_method_simple == "TruSeq"),
        aes(x = lab, y = counts, fill = as.factor(replicate))) +
     geom_bar(stat = "identity", position = "dodge") +
     facet_wrap(~category, scales = "free_y", nrow=4) +
-    ggsave("figures/stats/bcbio_sum.pdf")
+    ggtitle("bcbio - TrueSeq") +
+    ggsave("figures/stats/bcbio_sum.png")
+ggplot(bcbio_stats %>% filter(grepl("_sum", category),
+                              lib_method_simple == "NEBNext"),
+       aes(x = lab, y = counts, fill = as.factor(replicate))) +
+    geom_bar(stat = "identity", position = "dodge") +
+    facet_wrap(~category, scales = "free_y", nrow=4) +
+    ggtitle("bcbio - NEBNext") +
+    ggsave("figures/stats/bcbio_nebnext_sum.png")
